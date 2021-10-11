7efcdc393cae3a0ba99cdea2fbe8a00d8428a7dd
Meghan Krupka <mkrupka.hdm@gmail.com>
2021-10-11 08:25:21 -0400

    jQuery for modal and modal header style changes sass

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+// play/pause toggle functionality for carousel
 $(function() {
     $(".carousel").carousel( { interval: 2000 } );
     $("#carouselButton").click(function(){
@@ -12,3 +14,17 @@ $(function() {
         }
     });
 });
+
+// functionality for Reserve Campsite button modal on click
+$("#reserveButton").click(function() {
+    //console.log("hello");
+    $("#reserveModal").modal("toggle");
+})
+
+
+
+// functionality for Login button modal on click
+$("#loginButton").click(function() {
+    //console.log("hello");
+    $("#loginModal").modal("toggle");
+})
